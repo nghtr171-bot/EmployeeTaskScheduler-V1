4225395ee4437636056658288f1450d2e465c2ee
Ng Huu Trong <nghtr171@gmail.com>
2026-07-23 09:28:19 +0700

    Done project layout and features

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__, template_folder="app/templates")
 def home():
     return render_template("dashboard/index.html")
 
+@app.route("/about.html")
+def about():
+    return render_template("base.html")
+
+@app.route("/employees.html")
+def employees():
+    return render_template("dashboard/index.html")
+
+@app.route("/tasks.html")
+def tasks():
+    return render_template("dashboard/index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
